f7192611010d216838f02f52620a00668afa91bf
Jose Costa Teixeira <16153168+costateixeira@users.noreply.github.com>
2026-03-01 03:02:52 +0100

    minor fix in display

--- tx/library/renderer.js
+++ tx/library/renderer.js
@@ -438,7 +438,7 @@ class Renderer {
           }
         }
       } else {
-        li.tx(this.translate('VALUE_SET_CODES_FROM'));
+        li.tx(this.translate('VALUE_SET_CODES_FROM')+" ");
         await this.renderLink(li,inc.system+(inc.version ? "|"+inc.version : ""));
         li.tx(" "+ this.translate('VALUE_SET_WHERE')+" ");
         li.startCommaList("and");
